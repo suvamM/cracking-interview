0e9711bbb47f69d53bb7d666884610b69e89bcaa
Suvam Mukherjee <t-sumukh@microsoft.com>
2020-02-19 14:45:27 +0530

    updates

--- recursion/RecursionSolutions.java
+++ recursion/RecursionSolutions.java
@@ -54,11 +54,57 @@ public class RecursionSolutions
         }
     }
 
+    private ArrayList<String> getPerms (String str)
+    {
+        if (str == null) return null;
+
+        ArrayList<String> permutations = new ArrayList<String>();
+
+        if(str.length() == 0) {
+            permutations.add("");
+            return permutations;
+        }
+
+        char first = str.charAt(0); // get first character
+        String remainder = str.substring(1);
+
+        ArrayList<String> words = getPerms(remainder);
+
+        for(String word : words)
+        {
+            for(int j=0; j<=word.length(); j++)
+            {
+                String s = insertCharAt(word, first, j);
+                permutations.add(s);
+            }
+        }
+
+        return permutations;
+    }
+
+    private String insertCharAt (String word, char c, int index)
+    {
+        String start = word.substring(0, index);
+        String end = word.substring(index);
+        return start + c + end;
+    }
+
+    private void PrintAllPermutations (String s)
+    {
+        ArrayList<String> permutations = getPerms(s);
+
+        for(String word : permutations)
+        {
+            System.out.println(word);
+        }
+    }
+
     public static void main(String[] args)
     {
         // print powerset of set of integers
         RecursionSolutions r = new RecursionSolutions();
         r.PrintAllSubsets();
+        r.PrintAllPermutations("abc");
     }
 }
 
